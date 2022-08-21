97fea6bb52a0faa2b3b32287c5f78a737ed08f48
github-action-benchmark <github@users.noreply.github.com>
2022-08-21 15:13:30 +0000

    add "with ts-gql-plugin" vs "without ts-gql-plugin" Benchmark (benchmarkjs) benchmark result for 8a6d1e59fe59476d2129fd8447f8bb1bfd638484

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1661094810257,
+  "repoUrl": "https://github.com/Chnapy/ts-gql-plugin",
+  "entries": {
+    "\"with ts-gql-plugin\" vs \"without ts-gql-plugin\" Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Chnapy",
+            "username": "Chnapy"
+          },
+          "committer": {
+            "name": "Chnapy",
+            "username": "Chnapy"
+          },
+          "id": "8a6d1e59fe59476d2129fd8447f8bb1bfd638484",
+          "message": "Fix diagnostics keeped on changes / add benchmark",
+          "timestamp": "2022-06-15T13:57:54Z",
+          "url": "https://github.com/Chnapy/ts-gql-plugin/pull/23/commits/8a6d1e59fe59476d2129fd8447f8bb1bfd638484"
+        },
+        "date": 1661094809464,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "performance impact %: \"with ts-gql-plugin\" vs \"without ts-gql-plugin\"",
+            "value": 17.38,
+            "range": "±1.99%",
+            "unit": "%",
+            "extra": "10 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
